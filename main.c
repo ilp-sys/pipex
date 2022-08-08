ba4f078532027ab340d3b00d2e1fa1dbf1d15e0b
jiwoo ahn <ilp_rincipe@naver.com>
2022-08-08 15:14:41 +0900

    crying..

--- main.c
+++ main.c
@@ -6,23 +6,31 @@
 /*   By: jiwahn <jiwahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 11:40:55 by jiwahn            #+#    #+#             */
-/*   Updated: 2022/08/05 20:20:43 by jiwahn           ###   ########.fr       */
+/*   Updated: 2022/08/08 13:44:45 by jiwahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "pipex.h"
 
 int	main(int argc, char *argv[], char *argp[])
 {
-	int	fd1;
-	int	fd2;
+	int		fds[2];
+	pid_t	pid;
 
-	if (argc > 4)
+	if (argc == 5)
 	{
-		fd1 = check_heredoc(argv[1]); //error handling (negative fd)
-		while ()
-		{
+		if (pipe(fds))
+			return ;
 
+		pid = fork();
+		if (pid == (pid_t)0)
+		{
+			dup2(fd[1], STDIN_FILENO);
+			close(fd[1]);
+			close(fd[0]);
+			execve(); //
 		}
+
+		waitpid(pid, NULL, 0);
 	}
 	return 0;
 }
