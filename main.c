4506c7d36705f7f99132a345572d293d9e0038b0
jiwoo ahn <ilp_rincipe@naver.com>
2022-08-11 01:47:36 +0900

    backup

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jiwahn <jiwahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 11:43:06 by jiwahn            #+#    #+#             */
-/*   Updated: 2022/08/10 23:07:47 by jiwahn           ###   ########.fr       */
+/*   Updated: 2022/08/11 01:38:54 by jiwahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,32 +54,62 @@ char	*get_pathname(char *envp[], char *cmd)
 int main(int argc, char *argv[], char *envp[])
 {
 	// ./piepx infile.txt cmd1 cmd2 ... cmdn outfile
+	fprintf(stderr, "argc %d, starting from 0\n", argc);
 	if (argc > 4)
 	{
-		int i = 1;
+		int i = 0;
 		int fd[2];
 		pipe(fd);
 
-		int tmp = open(argv[i], O_RDONLY);
-		dup2(tmp, STDIN_FILENO);
-		close(tmp);
-		while (i++ < argc - 1)
+		fprintf(stderr, "main process %d\n", getpid());
+
+		while (++i < argc)
 		{
 			pid_t pid = fork();
 			if (pid == (pid_t)0)
 			{
-				dup2(fd[0], STDIN_FILENO);
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[0]);
-				close(fd[1]);
-				char **parsing = ft_split(argv[i], ' ');
-				execve(get_pathname(envp, parsing[0]), parsing, envp);
+				if (i == 1)
+				{
+					int tmp = open(argv[i], O_RDONLY);
+					dup2(tmp, STDIN_FILENO);
+					char buffer[100];
+					int ret = read(STDIN_FILENO, buffer, 100);
+					write(STDOUT_FILENO, buffer, ret);
+					dup2(fd[1], STDOUT_FILENO);
+					write(STDOUT_FILENO, buffer, ret);
+					close(tmp);
+					close(fd[0]);
+					close(fd[1]);
+					exit(EXIT_SUCCESS);
+				}
+				else if (i == argc -1)
+				{
+					int tmp2 = open(argv[i], O_RDWR | O_CREAT | O_TRUNC);
+					dup2(fd[0], STDIN_FILENO);
+					char buffer1[100];
+					int ret1 = read(STDIN_FILENO, buffer1, 100);
+					write(STDOUT_FILENO, buffer1, ret1);
+					dup2(tmp2, STDOUT_FILENO);
+					int ret2 = write(STDOUT_FILENO, buffer1, ret1);
+					fprintf(stderr, "%d bytes were written to ouftile.txt\n", ret2);
+					close(tmp2);
+					close(fd[0]);
+					close(fd[1]);
+					exit(EXIT_SUCCESS);
+				}
+				else
+				{
+					dup2(fd[0], STDIN_FILENO);
+					dup2(fd[1], STDOUT_FILENO);
+					close(fd[0]);
+					close(fd[1]);
+					char **parsing = ft_split(argv[i], ' ');
+					execve(get_pathname(envp, parsing[0]), parsing, envp);
+					exit(EXIT_FAILURE);
+				}
 			}
+			fprintf(stderr, "process %d generated, i was %d\n", pid, i);
 		}
-		int tmp2 = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC);
-		dup2(tmp2, STDOUT_FILENO);
-		close(tmp2);
-
 		close(fd[0]);
 		close(fd[1]);
 	}
--- pipex_utils.c
+++ pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: jiwahn <jiwahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 18:59:26 by jiwahn            #+#    #+#             */
-/*   Updated: 2022/08/09 19:00:40 by jiwahn           ###   ########.fr       */
+/*   Updated: 2022/08/11 00:44:13 by jiwahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@ void	sys_err_exit(char *msg)
 {
     perror(msg);
     exit(EXIT_FAILURE);
+}
+
+void	close_utils(int fd[2])
+{
+	close(fd[0]);
+	close(fd[1]);
 }
 
